aa62ec5c6ca85b692c87c6353bc438d944914bdd
ramyasri848 <kurakularamyasri3@gmail.com>
2026-07-21 14:59:59 +0530

    Implement role-based authorization

---
+++ server/middleware/roleMiddleware.js
@@ -0,0 +1,13 @@
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied. You do not have permission."
+      });
+    }
+
+    next();
+  };
+};
--- server/routes/protectedRoutes.js
+++ server/routes/protectedRoutes.js
@@ -1,14 +1,55 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
+import { authorizeRoles } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
+// Any logged-in user
 router.get("/profile", protect, (req, res) => {
-  res.status(200).json({
+  res.json({
     success: true,
-    message: "Protected route accessed successfully",
+    message: "Welcome!",
     user: req.user
   });
 });
+
+// Student only
+router.get(
+  "/student",
+  protect,
+  authorizeRoles("student"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Student"
+    });
+  }
+);
+
+// Faculty only
+router.get(
+  "/faculty",
+  protect,
+  authorizeRoles("faculty"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Faculty"
+    });
+  }
+);
+
+// Admin only
+router.get(
+  "/admin",
+  protect,
+  authorizeRoles("admin"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Admin"
+    });
+  }
+);
 
 export default router;
